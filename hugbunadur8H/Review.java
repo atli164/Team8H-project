5908e18af57b31a7a931b33e976c8f199a0d1768
atli164 <atli164@users.noreply.github.com>
2019-03-25 16:41:11 +0000

    Update Review.java

--- hugbunadur8H/Review.java
+++ hugbunadur8H/Review.java
@@ -6,7 +6,9 @@ public class Review {
 
   private int id;
   private int userId;
+  private User author;
   private int hotelId;
+  private Hotel aboutHotel;
   private int stars;
   private String content;
   private Date leftAt;
@@ -14,29 +16,44 @@ public class Review {
   public int getId() {
     return id;
   }
+  
   public int getUserId() {
     return userId;
   }
+  
+  public User getAuthor() {
+    return author;
+  }
+  
   public int getHotelId() {
     return hotelId;
   }
+  
+  public Hotel getAboutHotel() {
+    return aboutHotel;
+  }
+  
   public int getStars() {
     return stars;
   }
+  
   public String getContent() {
     return content;
   }
+  
   public Date getLeftAt() {
     return leftAt;
   }
 
-  public Review(int id, int userId, int hotelId, int stars, String content, Date leftAt) {
+  public Review(int id, int userId, int hotelId, int stars, String content, Date leftAt, User author, Hotel aboutHotel) {
     this.id = id;
     this.userId = userId;
     this.hotelId = hotelId;
     this.stars = stars;
     this.content = content;
     this.leftAt = leftAt;
+    this.author = author;
+    this.aboutHotel = aboutHotel;
   }
 
 
