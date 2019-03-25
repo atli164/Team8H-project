ffb093a2adae6a0f24af50d1cd4e43bcbb1b3b15
atli164 <atli164@users.noreply.github.com>
2019-03-25 16:36:11 +0000

    Add files via upload

---
+++ hugbunadur8H/User.java
@@ -0,0 +1,19 @@
+package hugbunadur8H;
+
+public class User {
+    private int id;
+    private String userName;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public User(int _id, String _userName) {
+        id = _id;
+        userName = _userName;
+    }
+}
