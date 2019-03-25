8d90efababfdd56f3ed637d891068891cff75b8b
Your Name <olieinar@hotmail.com>
2019-03-25 16:53:43 +0000

    booking reg

--- hugbunadur8H/BookingRegistry.java
+++ hugbunadur8H/BookingRegistry.java
@@ -1,5 +1,13 @@
 package hugbunadur8H;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class BookingRegistry {
+
+  //private ArrayList<Booking> bookings;
+  public ArrayList<Booking> getBookings(Date fromDate, Date toDate, Room room) {
+    return null;
+  }
 
 }
