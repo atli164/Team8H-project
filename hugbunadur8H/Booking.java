518fed0d6cb1325e5a414bc564e7dc9fdfe28e3c
atli164 <atli164@users.noreply.github.com>
2019-03-25 16:39:30 +0000

    Update Booking.java

--- hugbunadur8H/Booking.java
+++ hugbunadur8H/Booking.java
@@ -4,7 +4,9 @@ import java.util.Date;
 public class Booking{
     private int id;
     private int userId;
+    private User bookerUser;
     private int roomId;
+    private Room bookedRoom;
     private Date fromDate;
     private Date toDate;
     private Date requests;
@@ -12,24 +14,38 @@ public class Booking{
     public int getUserId(){
         return userId;
     }
+    
+    public User getBookerUser() {
+        return bookerUser;
+    }
+    
     public int getroomId(){
         return roomId;
     }
+    
+    public Room getBookedRoom() {
+        return bookedRoom;
+    }
+    
     public Date getFromDate(){
         return fromDate;
     }
+    
     public Date getToDate(){
         return toDate;
     }
+    
     public String getRequests(){
         return requests;
     }
 
-    public Booking(int id, int roomId, Date fromDate, Date toDate, String requests){
+    public Booking(int id, int roomId, Date fromDate, Date toDate, String requests, User bookerUser, Room bookedRoom){
         this.id = id;
         this.roomId = roomId;
         this.fromDate = fromDate;
         this.toDate = toDate;
         this.requests = requests;
+        this.bookerUser = bookerUser;
+        this.bookedRoom = bookedRoom;
     }
 }
